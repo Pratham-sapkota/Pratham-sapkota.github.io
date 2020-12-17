fa2ae02d4fcbf8ebc023bc1c5acefbf8923b10b1
Pratham-sapkota <prathamsapkota1837@gmail.com>
2020-12-17 12:35:42 +0545

    First commit

---
+++ hamburger.js
@@ -0,0 +1,9 @@
+
+
+const menuIcon =document.querySelector('.hamburger-menu');
+const navbar=document.querySelector('.box-2')
+menuIcon.addEventListener('click' ,()=>{
+ navbar.classList.toggle('change');
+});
+
+
